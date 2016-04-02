334dd49eebfd03036bdaed684ab013626821e657
Fu Wen <fu.wen@duke.edu>
2016-04-02 12:34:00 -0400

    join by event id

--- datacleaning.R
+++ datacleaning.R
@@ -31,3 +31,28 @@ stateFM = c("Georgia", "New York", "Miami", "Columbus", "Washington DC", "Michig
             "Minnesota", "Wisconsin", "Texas", "Louisiana", "Canada", "Alabama", "New Mexico",
             "North Carolina", "Virginia", "Canada", "Missouri", "Pennsylvania", "Tennessee",
             "Hawaii", "Canada", "Canada", "Canada", "Canada", "Alaska", "Utah")
+            
+## use sql
+con = dbConnect(RSQLite::SQLite(), ":memory:")
+
+#add up #of purchases and #of tickets according to event id
+dbWriteTable(con, "purchase", purchase)   # data frame -> database table
+query1 <- "SELECT event_id, 
+SUM(CASE WHEN tickets_purchased_qty > 0 THEN 1 ELSE 0 END) AS nBuy, 
+SUM(tickets_purchased_qty) AS nTickets FROM purchase GROUP BY event_id"
+res1 <- dbSendQuery(con, query1) 
+db1 <- dbFetch(res1, n=-1)  # n=-1 means return all
+
+#add up total hits for each event
+dbWriteTable(con, "ga", ga)   # data frame -> database table
+query2 <- "SELECT event_id,  
+SUM(totals_hits) AS nhits FROM ga GROUP BY event_id"
+res2 <- dbSendQuery(con, query2) 
+db2 <- dbFetch(res2, n=-1)  # n=-1 means return all
+
+#join two models according to event id
+dbWriteTable(con, "db1", db1) 
+dbWriteTable(con, "db2", db2)
+query3 <- "SELECT * FROM db1 JOIN db2 ON db1.event_id = db2.event_id"
+res3 <- dbSendQuery(con, query3) 
+db3 <- dbFetch(res3, n=-1) 
