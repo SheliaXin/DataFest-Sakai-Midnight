5df2c312c25729b551deb9f44049819f46bd520b
Leo Shu <lrshum17@outlook.com>
2016-04-02 12:07:24 -0400

    hello

--- datacleaning.R
+++ datacleaning.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(RSQLite)
 library(data.table)
 purchase <- fread("approved_data_purchase-v5.csv")
-
+ga = fread("approved_ga_data_v2.csv")
 
 ## use sql
 con = dbConnect(RSQLite::SQLite(), ":memory:")
@@ -20,3 +20,14 @@ number_v2 = number_v %>% filter(nBuy > 1)
 
 head(number_v2)
 hist(number_v2$nBuy)
+
+unique(purchase$fin_mkt_nm)
+
+stateFM = c("Georgia", "New York", "Miami", "Columbus", "Washington DC", "Michigan",
+            "California", "Texas", "Texas", "Pennsylvania", "Texas", "California",
+            "Massachusetts", "Missouri", "Florida", "Califonia", "Nevada", "North Carlonina",
+            "New York", "Arizona", "Florida", "Canada", "Canada", "Canada", "Iowa",
+            "Colorado", "Kentucky", "Canada", "Ohio", "Texas", "Ohio", "Oregon", "Washington",
+            "Minnesota", "Wisconsin", "Texas", "Louisiana", "Canada", "Alabama", "New Mexico",
+            "North Carolina", "Virginia", "Canada", "Missouri", "Pennsylvania", "Tennessee",
+            "Hawaii", "Canada", "Canada", "Canada", "Canada", "Alaska", "Utah")
