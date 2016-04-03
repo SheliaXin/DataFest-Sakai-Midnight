085868433446b8c595aa016ee645e869b9705b26
janejzyu <yujiangzhen@hotmail.com>
2016-04-02 20:09:08 -0400

    market segment and ticket/purchase data and plot added

--- code/visual.R
+++ code/visual.R
@@ -20,4 +20,36 @@ png(filename="image/venue.png")
 p
 dev.off()
 
-#
+# market segment by state
+load("rdata/mgStateFreq.rdata")
+
+pMkSeg = qplot(long, lat, data = mgStateFreq , 
+               group = group,
+               fill = total,
+               geom = "polygon")
+pMkSeg = pMkSeg + scale_fill_continuous(low = "khaki1", high = "indianred4", guide="colorbar")
+pMkSeg = pMkSeg + theme_bw()  + 
+         labs(fill = "total" 
+         ,title = "number of purchase by market in the U.S. by state", x="", y="")
+pMkSeg = pMkSeg + geom_path(data = mgStateFreq, colour = "grey", size = 0.1)
+png(filename="image/mkSeg.png")
+pMkSeg
+dev.off()
+
+
+#Ticket per purchase by state
+load("rdata/stateBuyTicket.rdata")
+pTP = qplot(long, lat, data = stateBuyTicket, 
+            group = group,
+            fill = ticketPerBuy,
+            geom = "polygon")
+
+pTP = pTP + scale_fill_continuous(low = "khaki1", high = "indianred4", guide="colorbar")
+pTP = pTP + theme_bw()  + 
+  labs(fill = "ticket/purchase" 
+       ,title = "Average ticket per purchase in U.S.", x="", y="") 
+pTP = pTP + geom_path(data = stateBuyTicket , 
+                      colour = "grey", size = 0.1)
+png(filename="image/ticket_purchase.png")
+pTP
+dev.off()
